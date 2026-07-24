08ef3c839de9ebe8c8e35e0a299bc690a8e2f9ef
Susmita Roy <sushrits@gmail.com>
2026-07-24 15:57:23 +0200

    Remove Mini_Assignment_Week3 folder

--- Mini_Assignment_Week3/sql_scripts/06_indexes.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX idx_orders_customer
-ON orders(customer_id);
-
-
-CREATE INDEX idx_orders_orderdate
-ON orders(order_date);
-
-CREATE INDEX idx_orders_customer_date
-ON orders(customer_id,order_date);
